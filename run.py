2efa70ecd31b62d0886bad4db6c2f5498ba0361d
gnudrew <gnudrew@gmail.com>
2020-12-19 02:48:22 -0600

    remove unused templates

--- run.py
+++ run.py
@@ -7,29 +7,5 @@ def home():
     # return "Index Page"
     return render_template("index.html")
 
-@app.route('/MoodboosterMobileApp')
-def app1():
-    return render_template("app1.html")
-
-@app.route('/DiningPhilosophers')
-def app2():
-    return render_template("app2.html")
-
-@app.route('/DiningPhilosophers')
-def app3():
-    return render_template("app3.html")
-
-@app.route('/DiningPhilosophers')
-def app4():
-    return render_template("app4.html")
-
-@app.route('/DiningPhilosophers')
-def app5():
-    return render_template("app5.html")
-
-@app.route('/DiningPhilosophers')
-def app6():
-    return render_template("app6.html")
-
 if __name__=="__main__":
     app.run(debug=True)
